df5b791fb002f00de14e91c6dd74351f17f977df
Anil Shafeeque <anil.shafeeque@gmail.com>
2020-10-24 14:27:42 +0300

    Call Logic app added

--- TwiliohttpTriggerCS.cs
+++ TwiliohttpTriggerCS.cs
@@ -24,39 +24,71 @@ namespace Company.Function
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            
-       // var data = await req.Content.ReadAsStringAsync();
-         var data = await new StreamReader(req.Body).ReadToEndAsync();
-
-            log.LogInformation(data);
+            //var data = await req.Content.ReadAsStringAsync();
+            var data = await new StreamReader(req.Body).ReadToEndAsync();
             var formValues = data.Split('&')
                 .Select(value => value.Split('='))
                 .ToDictionary(pair => Uri.UnescapeDataString(pair[0]).Replace("+", " "), 
-                            pair => Uri.UnescapeDataString(pair[1]).Replace("+", " "));
+                      pair => Uri.UnescapeDataString(pair[1]).Replace("+", " "));
 
-            // Perform calculations, API lookups, etc. here
+    // Perform calculations, API lookups, etc. here
 
             var quest = formValues["Body"];
             log.LogInformation(quest); 
 
-            
+    ////////////////////////////
 
+        
+            var result = await GetFAQAnswer(quest);
+          
+         
+
+    //////////////////////////////////
 
 
 
             log.LogInformation($"The message is {formValues["Body"]}");
 
             var response = new MessagingResponse()
-                .Message($"This is Shamla teacher , I am here to help you. {formValues["Body"]}");
+              .Message(result);
             var twiml = response.ToString();
             twiml = twiml.Replace("utf-16", "utf-8");
 
+            // var resp = new OkObjectResult(twiml);
+ 
             return new ContentResult
             {
                 ContentType = "application/xml",
                 Content = twiml,
                 StatusCode = 200
             };
+            // return new OkObjectResult(twiml).ContentTypes("application/xml");
+
+            
         }
+
+
+        public static async Task<string> GetFAQAnswer(string user_query)
+        {
+        
+        var client = new HttpClient();
+        Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        dictionary.Add("question", user_query);
+
+        string json = JsonConvert.SerializeObject(dictionary);
+        var requestData = new StringContent(json, Encoding.UTF8, "application/json");
+        var url = "https://prod-81.eastus.logic.azure.com:443/workflows/b807d77ea57847669090f30f68d226a2/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=hHbqXcdMS7NToxmUa563AZX1i2jKi9pr4aeBZ0qZJ0I";
+        var qaresponse = await client.PostAsync(String.Format(url), requestData);
+        var answ = await qaresponse.Content.ReadAsStringAsync();
+
+
+        return answ;
+        }
+
+        
     }
+
+
+
+
 }
